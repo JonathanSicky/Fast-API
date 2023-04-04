57e6dac8e06c587ab02dd3912b1b78b765bad15a
Isenger <6010110697@psu.ac.th>
2023-04-04 08:47:14 +0700

    update variable / code

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 # Connection MySQL
-import mysql.connector
+# import mysql.connector
+
+from product import get_products
+from productid import get_productid
 
 app = FastAPI()
 origins = ["*"]
@@ -24,120 +27,47 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-mySQL = None
-myCursor = None
-
-# Function Connect MySQL
-def mySQL_Connection():
-    global mySQL, myCursor
-    mySQL = mysql.connector.connect(host = "localhost", user = "root", password = "", database = "manageapi")
-    myCursor = mySQL.cursor(dictionary = True)
-
 @app.get("/api/products")
-def get_products(response: Response):
-    mySQL_Connection()
-
-    myCursor.execute('SELECT * FROM `products`')
-    mystringproducts = myCursor.fetchall()
-
-    myCursor.execute('SELECT * FROM `products_component`')
-    mystringcomponents = myCursor.fetchall()
-
-    products = []
-    for product in mystringproducts:
-        product_dict = {}
-        product_dict['product_id'] = product['product_id']
-        product_dict['product_name'] = product['product_name']
-        product_dict['product_price'] = product['product_price']
-        product_dict['product_cost'] = product['product_cost']
-        product_dict['product_total'] = product['product_total']
-        product_dict['product_detail'] = []
-
-        for component in mystringcomponents:
-            if component['component_id'] == product['product_id']:
-                component_dict = {}
-                component_dict['component_name'] = component['component_name']
-                component_dict['component_type'] = component['component_type']
-                product_dict['product_detail'].append(component_dict)
-        products.append(product_dict)
-        
-    mySQL.close()
-    myCursor.close()
-    response.status_code = status.HTTP_200_OK
-    return products
+def read_products():
+    products = get_products()
+    return JSONResponse(content=jsonable_encoder(products))
 
 @app.get("/api/products/{id}")
-def get_product(id: int, response: Response):
-    mySQL_Connection()
-
-    myproducts = 'SELECT * FROM `products` WHERE `product_id` = %s'
-    val = (id, )
-    myCursor.execute(myproducts, val)
-    mystringproducts = myCursor.fetchone()
-
-    mycomponents = 'SELECT * FROM `products_component` WHERE `component_id` = %s'
-    val = (id, )
-    myCursor.execute(mycomponents, val)
-    mystringcomponents = myCursor.fetchall()
-
-    if mystringproducts is None:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"message": "ไม่มีสินค้าชิ้นนี้ในฐานข้อมูล"}
-    else:
-        result = {}
-        result['product_id'] = mystringproducts['product_id']
-        result['product_name'] = mystringproducts['product_name']
-        result['product_price'] = mystringproducts['product_price']
-        result['product_total'] = mystringproducts['product_total']
-        
-        if mystringcomponents:
-            component = []
-            for data in mystringcomponents:
-                components = {}
-                components['name'] = data['component_name']
-                components['value'] = data['component_type']
-                component.append(components)
-            result['product_component'] = component
-        else:
-            result['product_component'] = None
-        
-        mySQL.close()
-        myCursor.close()
-        response.status_code = status.HTTP_200_OK
-        return result
+def read_product(id: int, response: Response):
+    return get_productid(id, response)
     
-@app.post("/api/product/post")
-async def insert_product(data: dict):
-    product_name = data.get("product_name")
-    product_price = data.get("product_price")
-    product_total = data.get("product_total")
-    product_cost = data.get("product_cost")
+# @app.post("/api/product/post")
+# async def insert_product(data: dict):
+#     product_name = data.get("product_name")
+#     product_price = data.get("product_price")
+#     product_total = data.get("product_total")
+#     product_cost = data.get("product_cost")
 
-    component_name = data.get("component_name")
-    component_type = data.get("component_type")
+#     # component_name = data.get("component_name")
+#     # component_type = data.get("component_type")
 
-    if not all([product_name, product_price, product_total, product_cost]):
-        raise HTTPException(status_code=400, detail="กรุณากรอกข้อมูลให้ครบถ้วนก่อนที่จะบันทึกลงฐานข้อมูล")
+#     if not all([product_name, product_price, product_total, product_cost]):
+#         raise HTTPException(status_code=400, detail="กรุณากรอกข้อมูลให้ครบถ้วนก่อนที่จะบันทึกลงฐานข้อมูล")
 
-    try:
-        # Connect to database
-        mySQL_Connection()
+#     try:
+#         # Connect to database
+#         mySQL_Connection()
         
-        # Insert `products` table
-        insertproducts = "INSERT INTO `products` (product_name, product_price, product_total, product_cost) VALUES (%s, %s, %s, %d)"
-        val = (product_name, product_price, product_total)
-        myCursor.execute(insertproducts, val)
+#         # Insert `products` table
+#         insertproducts = "INSERT INTO `products` (product_name, product_price, product_total, product_cost) VALUES (%s, %s, %s, %d)"
+#         val = (product_name, product_price, product_total)
+#         myCursor.execute(insertproducts, val)
 
-        # Insert `products_component` table
-        # insertcomponents = "INSERT INTO `products_component` (component_name, component_type) VALUES (%s, %d)"
-        # val = (component_name, component_type)
-        # myCursor.execute(insertcomponents, val)
+#         # Insert `products_component` table
+#         # insertcomponents = "INSERT INTO `products_component` (component_name, component_type) VALUES (%s, %d)"
+#         # val = (component_name, component_type)
+#         # myCursor.execute(insertcomponents, val)
 
-        # Commit changes and close connections
-        mySQL.commit()
-        myCursor.close()
-        mySQL.close()
+#         # Commit changes and close connections
+#         mySQL.commit()
+#         myCursor.close()
+#         mySQL.close()
 
-        return JSONResponse(content=jsonable_encoder({"message": "คุณได้เพิ่มสินค้าลงฐานข้อมูลเรียบร้อยแล้ว"}), status_code=201)
-    except mysql.connector.Error as err:
-        raise HTTPException(status_code=500, detail=str(err))
+#         return JSONResponse(content=jsonable_encoder({"message": "คุณได้เพิ่มสินค้าลงฐานข้อมูลเรียบร้อยแล้ว"}), status_code=201)
+#     except mysql.connector.Error as err:
+#         raise HTTPException(status_code=500, detail=str(err))
---
+++ product.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+def mySQL_Connection():
+    mySQL = mysql.connector.connect(host = "localhost", user = "root", password = "", database = "manageapi")
+    myCursor = mySQL.cursor(dictionary = True)
+    return mySQL, myCursor
+
+def get_products():
+    mySQL, myCursor = mySQL_Connection()
+
+    myCursor.execute('SELECT * FROM `products`')
+    mystringproducts = myCursor.fetchall()
+
+    myCursor.execute('SELECT * FROM `products_component`')
+    mystringcomponents = myCursor.fetchall()
+
+    products = []
+    for product in mystringproducts:
+        product_dict = {}
+        product_dict['product_id'] = product['product_id']
+        product_dict['product_name'] = product['product_name']
+        product_dict['product_price'] = product['product_price']
+        product_dict['product_cost'] = product['product_cost']
+        product_dict['product_total'] = product['product_total']
+        product_dict['product_detail'] = []
+
+        for component in mystringcomponents:
+            if component['component_id'] == product['product_id']:
+                component_dict = {}
+                component_dict['component_name'] = component['component_name']
+                component_dict['component_type'] = component['component_type']
+                product_dict['product_detail'].append(component_dict)
+        products.append(product_dict)
+        
+    mySQL.close()
+    myCursor.close()
+    
+    return products
